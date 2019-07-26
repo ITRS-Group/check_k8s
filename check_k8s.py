b5dd947dd24fdd1883d0fe0b3d5d3d72371c7f19
Robert Wikman <rbw@vault13.org>
2019-07-26 16:37:22 +0200

    Fix error handling

--- check_k8s.py
+++ check_k8s.py
@@ -3,27 +3,47 @@ import logging
 
 from urllib.error import URLError
 
-from k8s import Monitor, NagiosError, parse_cmdline
+from k8s import NagiosError, parse_cmdline, components
+from k8s.utils import build_url, http_request
+from k8s.consts import NAGIOS_MSG
+
+
+def nagios_msg(msg, channel=sys.stderr):
+    print(msg, file=channel)
 
 
 def main():
     args = parse_cmdline(sys.argv[1:])
 
     if args.debug:
-        logging.basicConfig(level=logging.DEBUG, format="%(levelname)s:%(message)s")
+        logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s] %(message)s")
 
-    monitor = Monitor(args.host, args.port, use_ssl=not args.no_ssl)
+    health_check, is_core = components.MAPPINGS[args.resource]
 
+    # Build URL using input arguments
+    url = build_url(
+        host=args.host,
+        port=args.port,
+        use_ssl=not args.no_ssl,
+        resource=args.resource,
+        is_core=is_core,
+        namespace=args.namespace
+    )
+
+    # Request and check health data
     try:
-        result = monitor.check_wrapped(args.resource, namespace=args.namespace)
-        sys.stdout.write(result)
+        response, status = http_request(url)
+        result = health_check(response)
+        nagios_msg(NAGIOS_MSG.format(level="OK", message=result), channel=sys.stdout)
     except NagiosError as e:
-        sys.stderr.write(e.message)
+        nagios_msg(NAGIOS_MSG.format(level=e.level, message=e.message))
         sys.exit(e.code)
     except URLError as e:
-        msg = "Server error: {}".format(str(e.reason))
-        sys.stderr.write(msg)
+        nagios_msg(NAGIOS_MSG.format(level="CRITICAL", message=e.reason))
         sys.exit(1)
+    except Exception as e:
+        nagios_msg(NAGIOS_MSG.format(level="UNKNOWN", message=e))
+        sys.exit(2)
 
 
 if __name__ == "__main__":
