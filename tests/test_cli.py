5f0634d502043f8b855ca3b15fdef78e5d86182a
Robert Wikman <rwikman@op5.com>
2019-07-31 14:39:41 +0200

    Add more CLI tests
    Signed-off-by: Robert Wikman <rbw@vault13.org>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,6 @@
-from functools import partial
-
 import pytest
 
-from k8s.cli import parse_cmdline
+from k8s.cli import parse_cmdline, Default
 
 
 def test_resource_valid():
@@ -21,6 +19,9 @@ def test_resource_invalid():
     with pytest.raises(SystemExit):
         parse_cmdline(['--resource', 'Deployments'])
 
+    with pytest.raises(SystemExit):
+        parse_cmdline([])
+
 
 def test_opts():
     def parser(args):
@@ -28,9 +29,12 @@ def test_opts():
 
     assert parser(["--namespace", "test"]).namespace == "test"
     assert parser(["--debug"]).debug is True
-    assert parser([]).debug is False
     assert parser(["--insecure"]).insecure is True
-    assert parser([]).insecure is False
     assert parser(["--timeout", "123.0"]).timeout == 123.0
     assert parser(["--port", "1234"]).port == 1234
     assert parser(["--token", "token123"]).token == "token123"
+    assert parser([]).debug is Default.debug.value
+    assert parser([]).insecure is Default.insecure.value
+    assert parser([]).timeout is Default.timeout.value
+    assert parser([]).port is Default.port.value
+    assert parser([]).host is Default.host.value
