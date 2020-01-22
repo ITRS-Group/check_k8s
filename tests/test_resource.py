8a170e51f93d515b195e9bc7d493167424b783da
Robert Wikman <rbw@vault13.org>
2020-01-22 18:44:31 +0100

    Use Node in base tests as Resource is now ABC
    Signed-off-by: Robert Wikman <rbw@vault13.org>

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,45 +1,45 @@
 import pytest
 
-from k8s.components.resource import Resource
+from k8s.components.node import Node
 from k8s.exceptions import MetaNotFound, StatusNotFound, ConditionsNotFound
 
 
 def test_no_meta(node_base):
     del node_base["metadata"]
     with pytest.raises(MetaNotFound):
-        Resource(node_base)
+        Node(node_base)
 
 
 def test_empty_meta(node_base):
     node_base["metadata"] = {}
     with pytest.raises(MetaNotFound):
-        Resource(node_base)
+        Node(node_base)
 
 
 def test_no_status(node_base):
     del node_base["status"]
     with pytest.raises(StatusNotFound):
-        Resource(node_base)
+        Node(node_base)
 
 
 def test_empty_status(node_base):
     node_base["status"] = {}
     with pytest.raises(StatusNotFound):
-        Resource(node_base)
+        Node(node_base)
 
 
 def test_no_conditions(node_base):
     with pytest.raises(ConditionsNotFound):
-        getattr(Resource(node_base), "condition")
+        getattr(Node(node_base), "condition")
 
 
 def test_empty_conditions(node_base):
     with pytest.raises(ConditionsNotFound):
-        getattr(Resource(node_base), "condition")
+        getattr(Node(node_base), "condition")
 
 
 def test_kind(node_full):
-    class Test(Resource):
+    class Test(Node):
         def _get_status(self, cnd_type, cnd_status):
             pass
 
@@ -47,7 +47,7 @@ def test_kind(node_full):
 
 
 def test_override_kind(node_full):
-    class Test(Resource):
+    class Test(Node):
         def __init__(self, data):
             super(Test, self).__init__(data, kind="test_kind")
 
