3feaecdfe7f504509324fcad6f8e8831a7a2623a
John Apple II <jappleii@redhat.com>
2022-02-10 16:19:45 +1100

    Fix error where namespace is not specified
    The system should have the namespace argument as None in order to have
    the monitoring script check all namespaces.

--- check_k8s.py
+++ check_k8s.py
@@ -23,35 +23,62 @@ def main():
     health_check, is_core = MAPPINGS[parsed.resource]
 
     response = []
-    for i in parsed.namespace.split(","):
-
-      # Build URL using input arguments
-      url = build_url(
-          host=parsed.host,
-          port=parsed.port,
-          resource=parsed.resource,
-          is_core=is_core,
-          namespace=i
-      )
-    # Request and check health data
-      try:
-          response_single, status = request(url, token=parsed.token, insecure=parsed.insecure)
-          response.extend(response_single);
-      except HTTPError as e:
-          body = json.loads(e.read().decode("utf8"))
-          output = Output(
-              NaemonState.UNKNOWN,
-              "{0}: {1}".format(e.code, body.get("message")),
-              sys.stdout
-          )
-      except URLError as e:
-          output = Output(NaemonState.UNKNOWN, e.reason, sys.stdout)
-      except Exception as e:
-          if parsed.debug:
-              exc_type, exc_value, exc_traceback = sys.exc_info()
-              traceback.print_tb(exc_traceback, file=sys.stdout)
-          output = Output(NaemonState.UNKNOWN, e, sys.stdout)
-
+    if parsed.namespace is not None:
+      for i in parsed.namespace.split(","):
+  
+        # Build URL using input arguments
+        url = build_url(
+            host=parsed.host,
+            port=parsed.port,
+            resource=parsed.resource,
+            is_core=is_core,
+            namespace=i
+        )
+      # Request and check health data
+        try:
+            response_single, status = request(url, token=parsed.token, insecure=parsed.insecure)
+            response.extend(response_single);
+        except HTTPError as e:
+            body = json.loads(e.read().decode("utf8"))
+            output = Output(
+                NaemonState.UNKNOWN,
+                "{0}: {1}".format(e.code, body.get("message")),
+                sys.stdout
+            )
+        except URLError as e:
+            output = Output(NaemonState.UNKNOWN, e.reason, sys.stdout)
+        except Exception as e:
+            if parsed.debug:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                traceback.print_tb(exc_traceback, file=sys.stdout)
+            output = Output(NaemonState.UNKNOWN, e, sys.stdout)
+    else:
+        # Build URL using input arguments
+        url = build_url(
+            host=parsed.host,
+            port=parsed.port,
+            resource=parsed.resource,
+            is_core=is_core,
+            namespace=None
+        )
+      # Request and check health data
+        try:
+            response_single, status = request(url, token=parsed.token, insecure=parsed.insecure)
+            response.extend(response_single);
+        except HTTPError as e:
+            body = json.loads(e.read().decode("utf8"))
+            output = Output(
+                NaemonState.UNKNOWN,
+                "{0}: {1}".format(e.code, body.get("message")),
+                sys.stdout
+            )
+        except URLError as e:
+            output = Output(NaemonState.UNKNOWN, e.reason, sys.stdout)
+        except Exception as e:
+            if parsed.debug:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                traceback.print_tb(exc_traceback, file=sys.stdout)
+            output = Output(NaemonState.UNKNOWN, e, sys.stdout)
 
 
     output = health_check(response).output
