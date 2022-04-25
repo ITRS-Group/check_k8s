961703b217b14a1a587afc81993ddebe679ecd94
Jerson Dumalaon <jdumalaon@itrsgroup.com>
2022-04-25 22:05:53 +0800

    Add command line paramerer to exclude specified resource names from the check result
    This commit adds the --ignore command line parameter to enable the users
    to ignore or exclude specified resource names from being included in the
    check result. The ignored resoures will not affect the final status being
    reported by the plugin.
    
    This resolves MON-13152.
    
    Signed-off-by: Jerson Dumalaon <jdumalaon@itrsgroup.com>

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -8,26 +8,50 @@ def test_kind(node_full):
     assert Node(node_full)._kind == "Node"
 
 
-def test_result(node_full):
-    result = check_nodes([node_full])
+def test_result(node_full, ignore_none):
+    result = check_nodes([node_full], ignore_none)
     assert isinstance(result, Result)
 
 
-def test_output(node_full):
-    output = check_nodes([node_full]).output
+def test_output(node_full, ignore_none):
+    output = check_nodes([node_full], ignore_none).output
     assert isinstance(output, Output)
 
 
-def test_ready(node_base, node_ready):
+def test_ready(node_base, node_ready, ignore_none):
     node_base["status"]["conditions"] = node_ready
-    assert check_nodes([node_base]).output.message.startswith(RESULT_SUCCESS)
+    assert check_nodes([node_base], ignore_none).output.message.startswith(RESULT_SUCCESS)
 
 
-def test_not_ready(node_base, node_not_ready):
+def test_ignore_ready(node_base, node_ready, ignore_all_node):
+    node_base["status"]["conditions"] = node_ready
+    message = check_nodes([node_base], ignore_all_node).output.message
+    lines = message.split('\n')
+    assert lines[0] == RESULT_SUCCESS
+    assert lines[1] == "|available=0 unavailable=0 degraded=0 unschedulable=0"
+
+
+def test_not_ready(node_base, node_not_ready, ignore_none):
     node_base["status"]["conditions"] = node_not_ready
-    assert check_nodes([node_base]).output.message.startswith(RESULT_CRITICAL)
+    assert check_nodes([node_base], ignore_none).output.message.startswith(RESULT_CRITICAL)
 
 
-def test_memory_pressure(node_base, node_memory_pressure):
+def test_ignored_not_ready(node_base, node_not_ready, ignore_all_node):
+    node_base["status"]["conditions"] = node_not_ready
+    message = check_nodes([node_base], ignore_all_node).output.message
+    lines = message.split('\n')
+    assert lines[0] == RESULT_SUCCESS
+    assert lines[1] == "|available=0 unavailable=0 degraded=0 unschedulable=0"
+
+
+def test_memory_pressure(node_base, node_memory_pressure, ignore_none):
     node_base["status"]["conditions"] = node_memory_pressure
-    assert check_nodes([node_base]).output.message.startswith(RESULT_WARNING)
+    assert check_nodes([node_base], ignore_none).output.message.startswith(RESULT_WARNING)
+
+
+def test_ignored_memory_pressure(node_base, node_memory_pressure, ignore_all_node):
+    node_base["status"]["conditions"] = node_memory_pressure
+    message = check_nodes([node_base], ignore_all_node).output.message
+    lines = message.split('\n')
+    assert lines[0] == RESULT_SUCCESS
+    assert lines[1] == "|available=0 unavailable=0 degraded=0 unschedulable=0"
--- tests/test_pods.py
+++ tests/test_pods.py
@@ -40,27 +40,54 @@ def test_container_count(pod_full, pod_containers):
     assert len(Pod(pod_full).containers) == len(pod_containers)
 
 
-def test_check_pending(pod_full):
+def test_check_pending(pod_full, ignore_none):
     pod_full["status"]["phase"] = "Pending"
-    assert check_pods([pod_full]).output.state == NaemonState.CRITICAL
+    assert check_pods([pod_full], ignore_none).output.state == NaemonState.CRITICAL
 
 
-def test_check_failed(pod_full):
+def test_check_failed(pod_full, ignore_none):
     pod_full["status"]["phase"] = "Failed"
-    assert check_pods([pod_full]).output.state == NaemonState.CRITICAL
+    assert check_pods([pod_full], ignore_none).output.state == NaemonState.CRITICAL
 
 
-def test_check_not_running(pod_base, pod_not_ready, pod_containers):
+def test_check_not_running(pod_base, pod_not_ready, pod_containers, ignore_none):
     pod_base["status"]["conditions"] = pod_not_ready
     pod_base["status"]["containerStatuses"] = pod_containers
-    assert check_pods([pod_base]).output.state == NaemonState.CRITICAL
+    assert check_pods([pod_base], ignore_none).output.state == NaemonState.CRITICAL
 
 
-def test_check_cond_other(pod_base, pod_condition_other, pod_containers):
+def test_check_not_running_ignored(pod_base, pod_not_ready, pod_containers, ignore_all_pod):
+    pod_base["status"]["conditions"] = pod_not_ready
+    pod_base["status"]["containerStatuses"] = pod_containers
+    output = check_pods([pod_base], ignore_all_pod).output
+    lines = output.message.split('\n')
+    assert output.state == NaemonState.OK
+    assert lines[1] == "|available=0 unavailable=0 degraded=0 pending=0"
+
+
+def test_check_cond_other(pod_base, pod_condition_other, pod_containers, ignore_none):
     pod_base["status"]["conditions"] = pod_condition_other
     pod_base["status"]["containerStatuses"] = pod_containers
-    assert check_pods([pod_base]).output.state == NaemonState.WARNING
+    assert check_pods([pod_base], ignore_none).output.state == NaemonState.WARNING
 
-def test_check_succeeded(pod_full):
+
+def test_check_cond_other_ignored(pod_base, pod_condition_other, pod_containers, ignore_all_pod):
+    pod_base["status"]["conditions"] = pod_condition_other
+    pod_base["status"]["containerStatuses"] = pod_containers
+    output = check_pods([pod_base], ignore_all_pod).output
+    lines = output.message.split('\n')
+    assert output.state == NaemonState.OK
+    assert lines[1] == "|available=0 unavailable=0 degraded=0 pending=0"
+
+
+def test_check_succeeded(pod_full, ignore_none):
     pod_full["status"]["phase"] = "Succeeded"
-    assert check_pods([pod_full]).output.state == NaemonState.OK
+    assert check_pods([pod_full], ignore_none).output.state == NaemonState.OK
+
+
+def test_check_succeeded_ignored(pod_full, ignore_all_pod):
+    pod_full["status"]["phase"] = "Succeeded"
+    output = check_pods([pod_full], ignore_all_pod).output
+    lines = output.message.split('\n')
+    assert output.state == NaemonState.OK
+    assert lines[1] == "|available=0 unavailable=0 degraded=0 pending=0"
